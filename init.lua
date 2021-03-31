94e85e4ad950b7256814b3de647e1c494d6fb3e9
Duane Robertson <duane@duanerobertson.com>
2021-03-31 09:24:19 -0500

    Change use_texture_alpha to new form.

--- init.lua
+++ init.lua
@@ -555,7 +555,7 @@ minetest.register_node(mod_name..':translocator', {
 	walkable = false,
 	paramtype = 'light',
 	paramtype2 = 'facedir',
-	use_texture_alpha = true,
+	use_texture_alpha = 'blend',
 	groups = {cracky = 3, oddly_breakable_by_hand = 3},
 	light_source = 13,
 	sounds = default.node_sound_glass_defaults(),
